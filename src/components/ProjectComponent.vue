0efab25eb310c3638a63c078b570d8b5a59654de
Artur Harutyunyan <arturhar@outlook.com>
2023-02-24 04:14:15 +0400

    a

---
+++ src/components/ProjectComponent.vue
@@ -0,0 +1,63 @@
+<template>
+  <section class="projects center" id="projects">
+    <div class="content">
+      <p data-aos="fade-down" data-aos-delay="100">FEATURED PROJECTS</p>
+      <div class="row center">
+        <div class="row-item" v-for="project in projects" :key="project.id">
+          <CardComponent
+            :liveURL="project.liveUrl"
+            :projectURL="project.projectUrl"
+            :title="project.name"
+            :description="project.description"
+            :tools="project.tools"
+            :delay="project.delay"
+          />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import CardComponent from "./CardComponent.vue";
+export default {
+  components: { CardComponent },
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapState(["projects"]),
+  },
+};
+</script>
+
+<style scoped>
+.projects {
+  min-height: 100vh;
+}
+p {
+  font-size: 18px;
+  line-height: 22px;
+  margin: 0 90px;
+}
+.row {
+  flex-flow: row wrap;
+  margin: 50px 0;
+}
+@media (max-width: 375px) {
+  .row-item {
+    width: 100%;
+  }
+}
+@media (max-width: 1234px) {
+  p {
+    margin: 35px 90px;
+  }
+}
+@media (max-width: 820px) {
+  p {
+    text-align: center;
+  }
+}
+</style>
---
+++ src/components/ToolComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <li class="triangle" data-aos="fade-down" :data-aos-delay="delay">
+    {{ name }}
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    name: { required: true, type: String },
+    delay: { required: true, type: Number },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
